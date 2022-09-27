eaa2871aa74d5b5925bfd5170e8448b27fdecba0
Alice Moretti <ali.moret88@gmail.com>
2022-09-27 15:30:52 +0300

    Added light mode to ExpenseItem.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,23 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
+import { useContext } from "react";
+import { DarkModeContext } from "../Context/LightModeContext";
 
 function ExpenseItem(props) {
+  const { lightMode } = useContext(DarkModeContext);
   const price = `€${props.amount}`;
   return (
     <li>
-      <Card className="expense-item">
+      <Card className={!lightMode ? "expense-item" : "expense-item-light"}>
         <ExpenseDate date={props.date} />
-        <div className="expense-item__description">
+        <div
+          className={
+            !lightMode
+              ? "expense-item__description"
+              : "expense-item-light__description"
+          }
+        >
           <h2>{props.title}</h2>
           <div className="expense-item__price">{price}</div>
         </div>
