67c7d4181c423cfd243725cfd29a0b5c0edaf615
Alice Moretti <ali.moret88@gmail.com>
2022-09-27 15:36:43 +0300

    Added light theme for expense date

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,11 +1,14 @@
 import "./ExpenseDate.css";
+import { useContext } from "react";
+import { DarkModeContext } from "../Context/LightModeContext";
 
 function ExpenseDate(props) {
+  const { lightMode } = useContext(DarkModeContext);
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
   return (
-    <div className="expense-date">
+    <div className={!lightMode ? "expense-date" : "expense-date-light"}>
       <div className="expense-date__month">{month}</div>
       <div className="expense-date__day">{day}</div>
       <div className="expense-date__year">{year}</div>
