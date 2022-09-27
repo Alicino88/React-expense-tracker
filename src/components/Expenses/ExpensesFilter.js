e7610f8e21ee16aedd8cbd502867fffaa5988118
Alice Moretti <ali.moret88@gmail.com>
2022-09-27 15:29:57 +0300

    Added light mode to ExpensesFilter.js

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./ExpensesFilter.css";
+import { useContext } from "react";
+import { DarkModeContext } from "../Context/LightModeContext";
 
 const ExpensesFilter = (props) => {
+  const { lightMode } = useContext(DarkModeContext);
   const filterYear = (e) => {
     let year = e.target.value;
     /*below we pass the state to the Expenses component as a function argument */
     props.onSelectYear(year);
   };
   return (
-    <div className="expenses-filter">
+    <div className={!lightMode ? "expenses-filter" : "expenses-filter-light"}>
       <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select value={props.visibleYear} onChange={filterYear}>
